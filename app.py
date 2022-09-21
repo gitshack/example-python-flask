cc3417228bdeca56aba8861bba7972e8a15dfbc7
GGedde <ggedde@users.noreply.github.com>
2022-09-21 11:39:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    return '<h1>This is the <b>Home</b> Page</h1><p><a href="/">Home</a></p><p><a href="/about">About</a></p>'
 
-@app.route('/hello')
+@app.route('/about')
 def hello():
-    return 'Hello, World'
+    return '<h1>This is the <b>About</b> Page</h1><p><a href="/">Home</a></p><p><a href="/about">About</a></p>'
